e9d0ec1f9db0e413fdd92dd483de52a5d46a9719
Karl Balemi <karl.balemi2@auspost.com.au>
2020-05-01 14:49:25 +1000

    Updates

---
+++ precache-manifest.de928e171c9df4b1be6709c79f864127.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a022d2e279085c31f56871111e4bac9",
+    "url": "/plakias-menu/index.html"
+  },
+  {
+    "revision": "43735fb646b8ba6f0f6a",
+    "url": "/plakias-menu/static/css/main.945b66ff.chunk.css"
+  },
+  {
+    "revision": "c1f74edaf75476e188fb",
+    "url": "/plakias-menu/static/js/2.489868c6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/plakias-menu/static/js/2.489868c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43735fb646b8ba6f0f6a",
+    "url": "/plakias-menu/static/js/main.723d66a2.chunk.js"
+  },
+  {
+    "revision": "8207478bd6316bd9f70e",
+    "url": "/plakias-menu/static/js/runtime-main.e68bcf2a.js"
+  },
+  {
+    "revision": "45192118ae3d05805798e04a41da3515",
+    "url": "/plakias-menu/static/media/crete-beach.45192118.jpg"
+  }
+]);
